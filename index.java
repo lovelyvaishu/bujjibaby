84b4bba4d5ca4e1029000b8004ba1f243bdf21a3
lovelyvaishu <121490472+lovelyvaishu@users.noreply.github.com>
2022-12-27 17:06:15 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<title>W3.JS</title>
+<meta charset="utf-8">
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<body>
+
+<h2>Testing W3.JS in HTML</h2>
+
+<h2>Sort List</h2>
+
+<p>Click the button to sort the list.</p>
+<p><button onclick="w3.sortHTML('#id01', 'li')">Sort</button></p>
+
+<ul id="id01">
+  <li>Oslo</li>
+  <li>Stockholm</li>
+  <li>Helsinki</li>
+  <li>Berlin</li>
+  <li>Rome</li>
+  <li>Madrid</li>
+</ul>
+
+</body>
+</html>
